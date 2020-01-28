17867f03c95560bd918d8952c99f953c6e4f0e6a
Ira <iritmeg@gmail.com>
2020-01-28 23:25:03 +0200

    files for submit

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,10 @@ var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename =require("gulp-rename");
+var imagemin =require("gulp-imagemin");
+var replace =require("gulp-replace");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -20,6 +24,44 @@ gulp.task("css", function () {
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
+
+gulp.task("build-css", function() {
+return gulp.src("source/less/style.less")
+.pipe(less())
+.pipe(postcss([
+      autoprefixer()
+    ]))
+.pipe(csso())
+.pipe(rename("style.min.css"))
+.pipe(gulp.dest("build/css/"));
+});
+
+
+gulp.task("build-images", function(){
+  return gulp.src("source/img/**/*.{svg,png,jpg}")
+
+.pipe(imagemin([
+
+imagemin.optipng({optimizationlevel: 3}),
+imagemin.mozjpeg(),
+imagemin.svgo()
+
+]))
+
+.pipe(gulp.dest("build/img"));
+
+});
+
+
+gulp.task("build-copy", function(){
+  return gulp.src("source/**/*.{html,woff,woff2,js}")
+  .pipe(replace('style.css', 'style.min.css'))
+  .pipe(gulp.dest("build/"));
+
+});
+
+
+
 
 gulp.task("server", function () {
   server.init({
@@ -35,3 +77,5 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("build", gulp.series("build-css", "build-images", "build-copy"));
